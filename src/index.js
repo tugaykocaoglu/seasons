50efb085d094d6cb61357a4b6afa4e5f71ebdfc8
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-04-28 20:19:10 +0300

    Render text due to statements.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Latitude: {position.coords.latitude}</div>;
-};
+    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO this.state !!
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // We called setState !!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render method !!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
